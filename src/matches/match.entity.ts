c4c910e40b3c57305f35603158c2ef031c586526
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:37:12 +0300

    🔧 Fixed Match Entity

--- src/matches/match.entity.ts
+++ src/matches/match.entity.ts
@@ -1,12 +1,31 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-import { MatchEntity } from "src/@types/Match/entity";
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ObjectIdColumn,
+} from "typeorm";
+import { MatchTypes } from "src/@types/Match/type";
+import { Exclude } from "class-transformer";
+import { ObjectID } from "mongodb";
 
 @Entity({ schema: "Match" })
-export class Match implements MatchEntity {
-  @PrimaryGeneratedColumn()
-  id;
-  @Column()
-  type;
-  @Column()
-  users;
+export class MatchEntity {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Exclude()
+    @ObjectIdColumn()
+    _id!: ObjectID;
+
+    @Column()
+    type: MatchTypes;
+
+    @Column()
+    status: boolean;
+
+    @Column()
+    users: { id: number }[];
+
+    @Column()
+    winner: { id: number };
 }
