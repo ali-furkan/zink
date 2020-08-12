40e307db49605f42d5f9d85528fddd675d73118f
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:32:43 +0300

    ⚙️ little bit adjustments

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,31 +1,34 @@
-import { Controller, Body, Post, Get, UseGuards, Req } from "@nestjs/common";
+import { Controller, Body, Post, Get, UseGuards } from "@nestjs/common";
 import { SignupDto } from "./dto/signup.dto";
 import { AuthorizeDto } from "./dto/authorize.dto";
 import { AuthService } from "./auth.service";
 import { IResponse } from "src/@types/Response/Response";
 import { GenerateTokenDto } from "./dto/generate-token.dto";
 import { AuthGuard } from "./auth.guard";
-import { Nest } from "src/@types/augmentation";
+import { User } from "src/users/user.decorator";
+import { ReqUser } from "src/@types/User/ReqUser";
+import { RateLimit } from "nestjs-rate-limit";
 
 @Controller("/auth")
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
-  @Post("/signup")
-  async signup(@Body() userBody: SignupDto): Promise<IResponse> {
-    return await this.authService.signup(userBody);
-  }
+    @RateLimit({ points: 10, duration: 60 * 60 })
+    @Post("/signup")
+    async signup(@Body() userBody: SignupDto): Promise<IResponse> {
+        return await this.authService.signup(userBody);
+    }
 
-  @Post("/authorize")
-  async authorize(
-    @Body() authBody: AuthorizeDto,
-  ): Promise<IResponse | GenerateTokenDto> {
-    return await this.authService.authorize(authBody);
-  }
+    @Post("/authorize")
+    async authorize(
+        @Body() authBody: AuthorizeDto,
+    ): Promise<IResponse | GenerateTokenDto> {
+        return await this.authService.authorize(authBody);
+    }
 
-  @UseGuards(AuthGuard)
-  @Get("/whoami")
-  whoami(@Req() req: Nest.Request): any {
-    return req.user;
-  }
+    @UseGuards(AuthGuard)
+    @Get("/whoami")
+    whoami(@User() user: ReqUser): ReqUser {
+        return user;
+    }
 }
