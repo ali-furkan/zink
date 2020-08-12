3acb474e065409659621d83ef5f2a85eff66ff7c
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:43:17 +0300

    🔥 Added Get Match

--- src/matches/match.controller.ts
+++ src/matches/match.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from "@nestjs/common";
+import {
+    Controller,
+    Put,
+    Get,
+    Param,
+    UseGuards,
+    Delete,
+    Body,
+    UseInterceptors,
+    ClassSerializerInterceptor,
+} from "@nestjs/common";
+import { TResponse } from "src/@types/Response/Response";
+import { AuthGuard } from "src/auth/auth.guard";
+import { User } from "src/users/user.decorator";
+import { ReqUser } from "src/@types/User/ReqUser";
+import { Flags } from "src/libs/flag.decorator";
+import { Flag } from "src/libs/snowflake";
+import { MatchService } from "./match.service";
+import { CreateMatchDTO } from "./dto/create-match.dto";
+import { GetMatchDto } from "./dto/get-match.dto";
+import { DeleteMatchDTO } from "./dto/delete-match.dto";
 
 @Controller("/matches")
-export class MatchController {}
+export class MatchController {
+    constructor(private matchService: MatchService) {}
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get("/:id")
+    @UseGuards(AuthGuard)
+    async getMatch(
+        @Param() params: GetMatchDto,
+        @User() user: ReqUser,
+    ): Promise<TResponse> {
+        return this.matchService.getMatch({ id: params.id, user });
+    }
+    
+}
--- src/matches/match.service.ts
+++ src/matches/match.service.ts
@@ -1,14 +1,50 @@
-import { Injectable } from "@nestjs/common";
+import {
+    Injectable,
+    HttpException,
+    HttpStatus,
+    ForbiddenException,
+    NotFoundException,
+    Inject,
+    forwardRef,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Match } from "./match.entity";
+import { MatchEntity } from "./match.entity";
 import { UserEntity } from "../users/user.entity";
+import { TResponse } from "src/@types/Response/Response";
+import { CreateMatchDTO } from "./dto/create-match.dto";
+import { MongoRepository } from "typeorm";
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
+import { ReqUser } from "src/@types/User/ReqUser";
+import { SnowflakeFactory, Type, Flag } from "src/libs/snowflake";
+import { UsersService } from "src/users/user.service";
 
 @Injectable()
 export class MatchService {
-  constructor(
-    @InjectRepository(Match)
-    private matchRepository: Match,
-    @InjectRepository(UserEntity)
-    private userRepository: UserEntity,
-  ) {}
+    private snowflake = new SnowflakeFactory([Flag.ACTIVE_USER], Type.USER);
+    constructor(
+        @InjectRepository(MatchEntity)
+        private matchRepository: MongoRepository<MatchEntity>,
+        @InjectRepository(UserEntity)
+        private userRepository: MongoRepository<UserEntity>,
+        @Inject(forwardRef(() => UsersService))
+        private userService: UsersService,
+    ) {}
+
+    async getMatch({
+        id,
+        user,
+    }: {
+        id: string;
+        user: ReqUser;
+    }): Promise<TResponse> {
+        const match = await this.matchRepository.findOne({ id });
+        const users = await match.users.map(async ({id})=>await this.userRepository.findOne({id}))
+        const SID = this.snowflake.serialization(user.id);
+        if (
+            !match.users.some(_user => _user.id === user.id) ||
+            SID.flags.includes("CREATE_MATCH")
+        )
+            throw new ForbiddenException();
+        return Object.assign({},match,{users});
+    }
 }
