e83f3586e7b038c2b30bdaaae6530270e04613d7
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:48:35 +0300

    Discriminator Added

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,49 +1,49 @@
 import {
-  Entity,
-  Column,
-  ObjectIdColumn,
-  PrimaryColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
+    Entity,
+    Column,
+    ObjectIdColumn,
+    PrimaryColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
 } from "typeorm";
 import { Exclude, Expose } from "class-transformer";
 import { ObjectID } from "mongodb";
 
 @Entity({ schema: "User" })
 export class UserEntity {
-  @PrimaryColumn()
-  id!: number;
+    @PrimaryColumn()
+    id!: number;
 
-  @Exclude()
-  @ObjectIdColumn()
-  _id: ObjectID;
+    @Exclude()
+    @ObjectIdColumn()
+    _id: ObjectID;
 
-  @Expose()
-  get tag(): string {
-    return `${this.username}#${this.discriminator}`;
-  }
+    @Expose()
+    get tag(): string {
+        return `${this.username}#${this.discriminator}`;
+    }
 
-  @Column({})
-  discriminator: number;
+    @Column()
+    discriminator: number;
 
-  @Column()
-  username: string;
+    @Column({ length: 32 })
+    username: string;
 
-  @Exclude()
-  @Column({ unique: true })
-  email: string;
+    @Exclude()
+    @Column({ unique: true })
+    email: string;
 
-  @Exclude()
-  @Column()
-  password: string;
+    @Exclude()
+    @Column()
+    password: string;
 
-  @CreateDateColumn({})
-  created_At: string;
+    @CreateDateColumn({})
+    createdAt: string;
 
-  @UpdateDateColumn({ type: "timestamp" })
-  updateAt: number;
+    @UpdateDateColumn({ type: "timestamp" })
+    updateAt: number;
 
-  constructor(partial: Partial<UserEntity>) {
-    Object.assign(this, partial);
-  }
+    constructor(partial: Partial<UserEntity>) {
+        Object.assign(this, partial);
+    }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,12 +1,12 @@
 import * as argon2 from "argon2";
 import { MongoRepository } from "typeorm";
 import {
-  Injectable,
-  Inject,
-  forwardRef,
-  HttpException,
-  HttpStatus,
-  NotFoundException,
+    Injectable,
+    Inject,
+    forwardRef,
+    HttpException,
+    HttpStatus,
+    NotFoundException,
 } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { SnowflakeFactory, Flag, Type } from "../libs/snowflake";
@@ -21,124 +21,157 @@ import { PatchUserDto } from "./dto/patch-user.dto";
 
 @Injectable()
 export class UsersService {
-  private snowflake = new SnowflakeFactory([Flag.ACTIVE_USER], Type.USER);
+    private snowflake = new SnowflakeFactory([Flag.ACTIVE_USER], Type.USER);
 
-  constructor(
-    @InjectRepository(UserEntity)
-    private userRepository: MongoRepository<UserEntity>,
-    @Inject(forwardRef(() => AuthService))
-    private authService: AuthService,
-  ) {}
+    constructor(
+        @InjectRepository(UserEntity)
+        private userRepository: MongoRepository<UserEntity>,
+        @Inject(forwardRef(() => AuthService))
+        private authService: AuthService,
+    ) {}
 
-  async getUsers(): Promise<UserEntity[]> {
-    const users = await this.userRepository.find();
-    return users;
-  }
+    async getUsers(): Promise<UserEntity[]> {
+        const users = await this.userRepository.find();
+        return users;
+    }
 
-  async isCorrectPassword({
-    email,
-    password,
-  }: {
-    email: string;
-    password: string;
-  }): Promise<[HttpException | null, UserEntity?]> {
-    const user = await this.userRepository.findOne({ email });
-    if (!user) return [new NotFoundException()];
-    const verify = await argon2.verify(user.password, password);
-    if (!verify)
-      return [new HttpException("Incorrect Password", HttpStatus.UNAUTHORIZED)];
-    return [null, user];
-  }
+    async isCorrectPassword({
+        email,
+        password,
+    }: {
+        email: string;
+        password: string;
+    }): Promise<[HttpException | null, UserEntity?]> {
+        const user = await this.userRepository.findOne({ email });
+        if (!user) return [new NotFoundException()];
+        const verify = await argon2.verify(user.password, password);
+        if (!verify)
+            return [
+                new HttpException(
+                    "Incorrect Password",
+                    HttpStatus.UNAUTHORIZED,
+                ),
+            ];
+        return [null, user];
+    }
 
-  async isUnique({ email }: { email: string }): Promise<boolean> {
-    const matchUsers = await this.userRepository.count({ email });
-    return matchUsers === 0;
-  }
+    async isUnique({ email }: { email: string }): Promise<boolean> {
+        const matchUsers = await this.userRepository.count({ email });
+        return matchUsers === 0;
+    }
 
-  async isExist({
-    id,
-  }: {
-    [propName: string]: any;
-  }): Promise<[boolean, UserEntity]> {
-    const user = await this.userRepository.findOne({ id });
-    return [user instanceof UserEntity, user];
-  }
+    async isExist({
+        id,
+    }: {
+        [propName: string]: any;
+    }): Promise<[boolean, UserEntity]> {
+        const user = await this.userRepository.findOne({ id });
+        return [user instanceof UserEntity, user];
+    }
 
-  /**
-   *
-   * @param param0
-   *
-   * @deprecated it's not recommended to use
-   * @todo add email Validation
-   * @todo new email validation
-   */
-  async editUser({
-    user,
-    patch,
-  }: {
-    user: ReqUser;
-    patch: PatchUserDto;
-  }): Promise<TResponse | GenerateTokenDto> {
-    const [err, oUser] = await this.isCorrectPassword({
-      email: user.email,
-      password: patch.password,
-    });
-    if (err) return err;
-    Object.assign(oUser, patch);
-    await this.userRepository.update({ id: user.id }, oUser);
-    const { access_token, expires_in } = this.authService.generateToken({
-      id: oUser.id,
-      email: oUser.email,
-    });
-    return {
-      message: "Successfully Patched User",
-      access_token,
-      expires_in,
-    };
-  }
+    /**
+     *
+     * @param param0
+     *
+     * @deprecated it's not recommended to use
+     * @todo add email Validation
+     * @todo new email validation
+     */
+    async editUser({
+        user,
+        patch,
+    }: {
+        user: ReqUser;
+        patch: PatchUserDto;
+    }): Promise<TResponse | GenerateTokenDto> {
+        const [err, oUser] = await this.isCorrectPassword({
+            email: user.email,
+            password: patch.password,
+        });
+        if (err) return err;
+        Object.assign(oUser, patch);
+        await this.userRepository.update({ id: user.id }, oUser);
+        const { access_token, expires_in } = this.authService.generateToken({
+            id: oUser.id,
+            email: oUser.email,
+        });
+        return {
+            message: "Successfully Patched User",
+            access_token,
+            expires_in,
+        };
+    }
 
-  async createUser({
-    username,
-    email,
-    password,
-  }: CreateUserDto): Promise<TResponse | GenerateTokenDto> {
-    const isUnique = await this.isUnique({ email });
-    if (!isUnique)
-      throw new HttpException(
-        "This email is already using",
-        HttpStatus.CONFLICT,
-      );
-    const hash = await argon2.hash(password);
-    const id = await this.snowflake.next();
-    const { access_token, expires_in } = this.authService.generateToken({
-      email,
-      id,
-    });
-    const user = this.userRepository.create({
-      id,
-      username,
-      email,
-      password: hash,
-    });
+    async createUser({
+        username,
+        email,
+        password,
+    }: CreateUserDto): Promise<TResponse | GenerateTokenDto> {
+        const isUnique = await this.isUnique({ email });
+        if (!isUnique)
+            throw new HttpException(
+                "This email is already using",
+                HttpStatus.CONFLICT,
+            );
+        const hash = await argon2.hash(password);
+        const id = await this.snowflake.next();
+        const { access_token, expires_in } = this.authService.generateToken({
+            email,
+            id,
+        });
+        const discriminator = await this.genDiscriminator(username);
+        const user = this.userRepository.create({
+            id,
+            username,
+            discriminator,
+            email,
+            password: hash,
+        });
 
-    await this.userRepository.save(user);
+        await this.userRepository.save(user);
 
-    return {
-      message: "Successfully Signup",
-      access_token,
-      expires_in,
-    };
-  }
+        return {
+            message: "Successfully Signup",
+            access_token,
+            expires_in,
+        };
+    }
 
-  async getUserData({
-    id,
-    email,
-  }: GetUserDto): Promise<TResponse & UserEntity> {
-    const [isExist, user] = await this.isExist({ id });
-    if (!isExist)
-      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
-    if (email && user.email !== email)
-      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
-    return Object.assign(user, { _id: undefined, password: undefined });
-  }
+    async genDiscriminator(username: string): Promise<number> {
+        const gen = () => {
+            let res = 0;
+            for (let i = 0; i < 4; i++) {
+                res += Math.floor(Math.random() * 10) * 10 ** i;
+            }
+            return res;
+        };
+        const uniqueDiscriminator = async (
+            username: string,
+        ): Promise<number> => {
+            const discriminator = gen();
+            const isExist: boolean =
+                (await this.userRepository.count({
+                    username,
+                    discriminator,
+                })) !== 0;
+            if (isExist) {
+                return await uniqueDiscriminator(username);
+            } else {
+                return discriminator;
+            }
+        };
+        return await uniqueDiscriminator(username);
+    }
+
+    async getUserData({
+        id,
+        email,
+    }: GetUserDto): Promise<TResponse & UserEntity> {
+        const [isExist, user] = await this.isExist({ id });
+        if (!isExist)
+            throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+        if (email && user.email !== email)
+            throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+        return Object.assign(user, { _id: undefined, password: undefined });
+    }
 }
