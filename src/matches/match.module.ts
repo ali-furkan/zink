98142447047fac9346ef6520efc6ac03da9d92a1
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:41:18 +0300

    🎊 Created Match Module

--- src/matches/match.module.ts
+++ src/matches/match.module.ts
@@ -1,14 +1,18 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { MatchController } from "./match.controller";
 import { MatchService } from "./match.service";
 import { UserEntity } from "../users/user.entity";
-import { Match } from "../matches/match.entity";
+import { MatchEntity } from "../matches/match.entity";
+import { UsersModule } from "src/users/user.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match, UserEntity])],
-  controllers: [MatchController],
-  providers: [MatchService],
-  exports: [TypeOrmModule],
+    imports: [
+        TypeOrmModule.forFeature([MatchEntity, UserEntity]),
+        forwardRef(() => UsersModule),
+    ],
+    controllers: [MatchController],
+    providers: [MatchService],
+    exports: [TypeOrmModule],
 })
 export class MatchModule {}
