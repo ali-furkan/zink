a057b33ce77efaeb98aab4eaa612e436de795a1e
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:28:13 +0300

    🧱 Added Rate Limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,38 +5,40 @@ import { UsersModule } from "src/users/user.module";
 import { MatchModule } from "src/matches/match.module";
 import Config from "./config";
 import { AuthModule } from "src/auth/auth.module";
-import { RateLimiterModule, RateLimiterGuard } from "nestjs-rate-limit"
+import { RateLimiterModule, RateLimiterGuard } from "nestjs-rate-limit";
 import { APP_GUARD } from "@nestjs/core";
+import { StatusModule } from "./status/status.module";
 @Module({
-  imports: [
-    RateLimiterModule.forRoot({
-      points: 100,
-      duration: 5,
-      keyPrefix: "global"
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [Config],
-    }),
-    TypeOrmModule.forRoot({
-      type: "mongodb",
-      url: Config().mongodbURI,
-      database: "Zink-DB",
-      synchronize: true,
-      logger: "debug",
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      autoLoadEntities: true,
-    }),
-    UsersModule,
-    MatchModule,
-    AuthModule,
-  ],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: RateLimiterGuard
-    }
-  ]
+    imports: [
+        RateLimiterModule.forRoot({
+            points: 100,
+            duration: 5,
+            keyPrefix: "global",
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [Config],
+        }),
+        TypeOrmModule.forRoot({
+            type: "mongodb",
+            url: Config().mongodbURI,
+            database: "Zink-DB",
+            synchronize: true,
+            logger: "debug",
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            autoLoadEntities: true,
+        }),
+        UsersModule,
+        MatchModule,
+        AuthModule,
+        StatusModule,
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: RateLimiterGuard,
+        },
+    ],
 })
-export class AppModule { }
+export class AppModule {}
