5be3fe150e0378f3bb7815a9c64931c775043644
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:29:22 +0300

    🔥 Added Response latency middleware

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,31 @@ import * as morgan from "morgan";
 import { AppModule } from "src/app.module";
 import Config from "src/config";
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+    FastifyAdapter,
+    NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { ValidationPipe } from "@nestjs/common";
+import * as cache from "memory-cache";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  app.use(helmet());
-  app.enableCors();
-  app.setGlobalPrefix(Config().rootPath);
-  app.use(morgan(Config().isProd ? "common" : "dev"))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  await app.listen(Config().port, "0.0.0.0");
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+    app.use(async (req, res, next) => {
+        cache.del("req.time");
+        cache.put("req.time", process.hrtime(), 5000);
+        await next();
+    });
+    app.use(helmet());
+    app.enableCors();
+    app.setGlobalPrefix(Config().rootPath);
+    app.use(morgan(Config().isProd ? "common" : "dev"));
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        }),
+    );
+    await app.listen(Config().port, "0.0.0.0");
 }
 bootstrap();
