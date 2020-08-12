5b437b5430dd5b9987bf6275f53a067321745c40
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:34:01 +0300

    🎌 Flag Check!

---
+++ src/libs/flag.decorator.ts
@@ -0,0 +1,8 @@
+import { Flag } from "./snowflake";
+import { SetMetadata } from "@nestjs/common";
+
+export const Flags = (...flags: Flag[]):any =>
+    SetMetadata(
+        "flags",
+        flags.reduce((acc, cur) => acc | cur),
+    );
