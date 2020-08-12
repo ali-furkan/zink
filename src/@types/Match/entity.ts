fc7a13830667754e4f96a173865dbea18a40e47e
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:55:06 +0300

    ⚙️ MatchEntity Adjustments

--- src/@types/Match/entity.ts
+++ src/@types/Match/entity.ts
@@ -1,10 +1,11 @@
 import { MatchTypes } from "./type";
 
 export interface MatchEntity {
-  id: number;
-  type: MatchTypes;
-  users: Array<{
     id: string;
-    name: string;
-  }>;
+    type: MatchTypes;
+    status: boolean;
+    winner: { id: number };
+    users: Array<{
+        id: number;
+    }>;
 }
