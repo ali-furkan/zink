35269a1bc2782be3d24684aa497d3b173ed35e90
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-12 15:30:45 +0300

    🔔 Declared Snowflake id on Request

--- src/@types/augmentation.d.ts
+++ src/@types/augmentation.d.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
 import { IToken } from "./User/token";
+import { Type, ISnowflake } from "src/libs/snowflake";
 
 declare namespace Nest {
-  export interface Request extends express.Request {
-    user: IToken;
-  }
-
-  export type Response = express.Response;
+    export interface Request extends express.Request {
+        user: IToken & { type: Type } & { SID: ISnowflake };
+    }
+    export type Response = express.Response;
 }
