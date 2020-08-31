b919b829ca5e576a87f41de5caef0047885628ed
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-08-31 09:40:03 +0300

    :bug:: fix dtos

--- src/status/dto/get-user.dto.ts
+++ src/status/dto/get-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsNumber, IsUUID, IsOptional } from "class-validator";
+import { Transform } from "class-transformer";
+
 export abstract class GetUserDto {
+    @IsNumber()
+    @Transform(v => Number(v))
+    @IsOptional()
     begin?: number;
+
+    @IsNumber()
+    @Transform(v => Number(v))
+    @IsOptional()
     length?: number;
-    id?: number;
+
+    @IsUUID(5)
+    @IsOptional()
+    id?: string;
 }
