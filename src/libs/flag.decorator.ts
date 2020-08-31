a31cf81eb8bf71012fb7e90390cf67c91d057e91
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-08-31 09:40:30 +0300

    :fire:: remove snowflake lib

--- src/libs/flag.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { Flag } from "./snowflake";
-import { SetMetadata } from "@nestjs/common";
-
-export const Flags = (...flags: Flag[]): any =>
-    SetMetadata(
-        "flags",
-        flags.reduce((acc, cur) => acc | cur),
-    );
