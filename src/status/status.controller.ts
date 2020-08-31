f4cff5eba4acb118025829a08de3b4fba014d523
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-08-31 09:38:34 +0300

    :sparkles:: create status module

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
-import { StatusService } from "./status.service";
-import { TResponse } from "src/@types/Response/Response";
-import { GetUserDto } from "src/users/dto/get-user.dto";
-import { GetMatchDto } from "./dto/get-match.dto";
-import { Flags } from "src/libs/flag.decorator";
-import { Flag } from "src/libs/snowflake";
-import { AuthGuard } from "src/auth/auth.guard";
 import * as cache from "memory-cache";
+import { StatusService } from "./status.service";
+import { AuthGuard } from "src/auth/auth.guard";
+import { Flags, Flag } from "src/auth/flag.decorator";
+import { GetUserDto } from "./dto/get-user.dto";
+import { GetMatchDto } from "./dto/get-match.dto";
 
 @Controller("/status")
 export class StatusController {
@@ -15,12 +13,12 @@ export class StatusController {
     @Flags(Flag.DEV)
     @UseGuards(AuthGuard)
     @Get("/system")
-    getSystemInfo(): TResponse {
+    getSystemInfo(): Zink.Response {
         return this.statusService.getInfo();
     }
 
     @Get(["/ping", "/"])
-    pingPong(): TResponse {
+    pingPong(): Zink.Response {
         const time = process.hrtime(cache.get("req.time"));
         return { pong: time[0] * 10 ** 9 + time[1] };
     }
@@ -28,14 +26,20 @@ export class StatusController {
     @Flags(Flag.DEV)
     @UseGuards(AuthGuard)
     @Get("/users")
-    async getUsers(@Query() query: GetUserDto): Promise<TResponse> {
+    async getUsers(@Query() query: GetUserDto): Promise<Zink.Response> {
         return await this.statusService.getUsers(query);
     }
 
     @Flags(Flag.DEV)
     @Get("/matches")
     @UseGuards(AuthGuard)
-    async getMatches(@Query() query: GetMatchDto): Promise<TResponse> {
+    async getMatches(@Query() query: GetMatchDto): Promise<Zink.Response> {
         return await this.statusService.getMatches(query);
+    }
+
+    @Flags(Flag.DEV)
+    @Get("/hosting")
+    async getStatus(): Promise<Zink.Response> {
+        return this.statusService.getHosting();
     }
 }
