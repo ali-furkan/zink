522abd3e2b4d7984cd8967df7eb17bb2c54d40ce
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-09-02 22:31:41 +0300

    :zap:: improve auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,9 +4,11 @@ import {
     ExecutionContext,
     Inject,
     forwardRef,
+    HttpStatus,
 } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import * as jwt from "jsonwebtoken";
+import * as cache from "memory-cache";
 import Config from "../config";
 import { UsersService } from "../users/user.service";
 
@@ -17,6 +19,20 @@ export class AuthGuard implements CanActivate {
         private userService: UsersService,
         private reflector: Reflector,
     ) {}
+
+    errHandler(message: string, res: { [prop: string]: any }): void {
+        res.status(HttpStatus.UNAUTHORIZED).send({
+            err: { message },
+        });
+    }
+
+    succHandler(
+        req: { [prop: string]: any },
+        data: { [prop: string]: any },
+    ): void {
+        req.user = { ...data };
+    }
+
     async canActivate(ctx: ExecutionContext): Promise<boolean> {
         const flags = this.reflector.get<number>("flags", ctx.getHandler());
         const [req, res] = [
@@ -39,16 +55,34 @@ export class AuthGuard implements CanActivate {
             token,
             Config().secret,
             async (err, decoded: Zink.IToken) => {
-                if (err)
-                    return res
-                        .status(401)
-                        .send({ err: { message: "Invalid Token" } });
-                req.user = { ...decoded };
+                if (err) return this.errHandler("Invalid Token", res);
+                if (cache.get(decoded.id) === false)
+                    return this.errHandler(
+                        "Unauthorized Request: User Not Found",
+                        res,
+                    );
+                else if (cache.get(decoded.id) === true)
+                    this.succHandler(req, decoded);
+                else {
+                    const [isExist, user] = await this.userService.isExist(
+                        decoded.id,
+                    );
+                    cache.put(
+                        decoded.id,
+                        isExist && user.email === decoded.email ? true : false,
+                        24 * 60 * 60 * 1000,
+                    );
+                    if (!isExist || user.email !== decoded.email)
+                        return this.errHandler(
+                            "Unauthorized Request: User Not Found",
+                            res,
+                        );
+                    cache.put(decoded.id, true, 24 * 60 * 60 * 1000);
+                    this.succHandler(req, decoded);
+                }
             },
         );
-        if (flags) {
-            return this.userService.matchFlags(flags, req.user.id);
-        }
+        if (flags) return this.userService.matchFlags(flags, req.user.flags);
         return true;
     }
 }
