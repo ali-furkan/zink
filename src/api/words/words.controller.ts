260c628f32f96ca27be09a15b4130b7b1c2701a7
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-09-10 23:51:50 +0300

    :fix:: add flag, auth in words

--- src/api/words/words.controller.ts
+++ src/api/words/words.controller.ts
@@ -5,12 +5,14 @@ import {
     Delete,
     Body,
     Query,
-    ParseIntPipe,
     Param,
+    UseGuards,
 } from "@nestjs/common";
 import { AddWordDto } from "./dto/add-word.dto";
 import { WordService } from "./words.service";
 import { WordEntity } from "./words.entity";
+import { AuthGuard } from "../auth/auth.guard";
+import { Flag, Flags } from "../auth/flag.decorator";
 
 @Controller("words")
 export class WordController {
@@ -28,6 +30,8 @@ export class WordController {
         return await this.wordService.getRandom(difficulty);
     }
 
+    @Flags(Flag.DEV)
+    @UseGuards(AuthGuard)
     @Get("/all")
     async getAllWord(
         @Query("begin") begin?: number,
@@ -36,11 +40,15 @@ export class WordController {
         return await this.wordService.getAll(begin, length);
     }
 
+    @Flags(Flag.DEV)
+    @UseGuards(AuthGuard)
     @Put()
     async addWord(@Body() wordBody: AddWordDto): Promise<Zink.Response> {
         return this.wordService.add(wordBody);
     }
 
+    @Flags(Flag.DEV)
+    @UseGuards(AuthGuard)
     @Delete("/id/:id")
     async delWord(@Param("id") id: string): Promise<WordEntity> {
         return await this.wordService.getID(id);
