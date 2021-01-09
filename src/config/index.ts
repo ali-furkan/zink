2b965ce0dfce9bb7190e1b3e2a9201964fe139f4
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-09 13:43:52 +0300

    :art: (Configs): Added Auto Secret Fetching

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,25 +1,67 @@
-import * as fs from "fs";
-
 const isProd = process.env.NODE_ENV === "production";
-if (isProd) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const env = require("dotenv").parse(fs.readFileSync("./prod.env"));
-    for (const k in env) {
-        process.env[k] = env[k];
+
+export const init = async () => {
+    if (!isProd) await (await import("dotenv")).config();
+
+    const impSecret = [
+        "MONGODB_URI",
+        "SG_KEY",
+        "FB_API_KEY",
+        "FB_AUTH_DOMAIN",
+        "FB_DB_URL",
+        "FB_PROJECT_ID",
+        "FB_STORAGE_BUCKET",
+        "FB_MSG_SENDER_ID",
+        "FB_APP_ID",
+        "FB_MSRMT_ID",
+        "UUID_NAMESPACE",
+        "SECRET_KEY",
+        "FLYIO_TOKEN",
+    ];
+
+    const envKeys = Object.keys(process.env);
+
+    if (impSecret.every(s => envKeys.includes(s))) return;
+
+    let { TOKEN, APP_NAME, KEY, BASE_URL } = process.env;
+
+    if(!TOKEN||!APP_NAME||!KEY||!BASE_URL) {
+        [BASE_URL,APP_NAME,KEY,TOKEN] = process.argv.splice(2)
     }
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-} else require("dotenv").config();
+
+    const getSecrets = (await import("../../scripts/secrets/get")).default;
+
+    const secrets = await getSecrets(BASE_URL, APP_NAME, KEY, TOKEN);
+
+    for (const k in secrets) {
+        process.env[k] = secrets[k];
+    }
+
+    if (!isProd) {
+        const fs = await import("fs");
+        const parserSecret = (() => {
+            let r = [];
+            for (const k in secrets) {
+                r.push(`${k}=${secrets[k]}`);
+            }
+            return r.join("\r\n");
+        })();
+        fs.writeFileSync("./.env", parserSecret, {
+            encoding: "utf-8",
+        });
+    }
+};
 
 export default (): Zink.TConfig => ({
     port: parseInt(process.env.PORT) || parseInt(process.argv[2]) || 3000,
     NODE_ENV: process.env.NODE_ENV || "development",
-    mongodbURI: process.env.MONGODB_URI || "",
+    mongodbURI: process.env.MONGODB_URI,
     secret: process.env.SECRET_KEY || "test",
     rootPath: process.env.ROOT_PATH,
     flyioToken: process.env.FLYIO_TOKEN,
     isProd,
     sgKey: process.env.SG_KEY,
-    mail: "noreply@zink-cloud.co",
+    mail: "noreply@zinkapp.co",
     UUID_NAMESPACE: process.env.UUID_NAMESPACE,
     Firebase: {
         apiKey: process.env.FB_API_KEY,
