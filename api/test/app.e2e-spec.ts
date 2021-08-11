079472492810b4594a729333ad5d2aa68b7dae1b
ali-furkan <a.furkan.2003@gmail.com>
2021-08-12 01:36:11 +0300

    :rotating_light:(api): format for new prettier rule

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,25 +1,25 @@
-import * as request from "supertest";
-import { Test } from "@nestjs/testing";
-import { AppModule } from "src/app.module";
-import { INestApplication } from "@nestjs/common";
-import { FastifyAdapter } from "@nestjs/platform-fastify";
+import * as request from "supertest"
+import { Test } from "@nestjs/testing"
+import { AppModule } from "src/app.module"
+import { INestApplication } from "@nestjs/common"
+import { FastifyAdapter } from "@nestjs/platform-fastify"
 
 describe("App (e2e)", () => {
-    let app: INestApplication;
+    let app: INestApplication
 
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
-        }).compile();
+        }).compile()
 
-        app = moduleRef.createNestApplication(new FastifyAdapter());
+        app = moduleRef.createNestApplication(new FastifyAdapter())
 
-        await app.init();
+        await app.init()
         await app
             .getHttpAdapter()
             .getInstance()
-            .ready();
-    });
+            .ready()
+    })
 
     it("Server should response with ping time when the '/api/v1/status/' request from clients", done => {
         return request(app.getHttpServer())
@@ -27,15 +27,15 @@ describe("App (e2e)", () => {
             .expect(200)
             .end((err, res) => {
                 if (err) {
-                    return done(err);
+                    return done(err)
                 }
-                expect(typeof res.body).toBe("object");
-                expect(typeof res.body.pong).toBe("number");
-                done();
-            });
-    });
+                expect(typeof res.body).toBe("object")
+                expect(typeof res.body.pong).toBe("number")
+                done()
+            })
+    })
 
     afterAll(async () => {
-        await app.close();
-    });
-});
+        await app.close()
+    })
+})
--- api/test/auth.e2e-spec.ts
+++ api/test/auth.e2e-spec.ts
@@ -1,14 +1,14 @@
-import * as request from "supertest";
-import { Test } from "@nestjs/testing";
-import { AppModule } from "src/app.module";
-import { INestApplication, HttpStatus } from "@nestjs/common";
-import { FastifyAdapter } from "@nestjs/platform-fastify";
+import * as request from "supertest"
+import { Test } from "@nestjs/testing"
+import { AppModule } from "src/app.module"
+import { INestApplication, HttpStatus } from "@nestjs/common"
+import { FastifyAdapter } from "@nestjs/platform-fastify"
 
 describe("Auth (e2e)", () => {
-    let app: INestApplication;
+    let app: INestApplication
 
-    let token: string;
-    let refreshToken: string;
+    let token: string
+    let refreshToken: string
 
     const data = {
         new: {
@@ -20,21 +20,21 @@ describe("Auth (e2e)", () => {
             email: "test@zinkapp.co",
             password: "qmbHecprgHAA3txP",
         },
-    };
+    }
 
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
-        }).compile();
+        }).compile()
 
-        app = moduleRef.createNestApplication(new FastifyAdapter());
+        app = moduleRef.createNestApplication(new FastifyAdapter())
 
-        await app.init();
+        await app.init()
         await app
             .getHttpAdapter()
             .getInstance()
-            .ready();
-    });
+            .ready()
+    })
 
     it("Server should response with successfully message when client registers with unique data", done => {
         return request(app.getHttpServer())
@@ -42,15 +42,15 @@ describe("Auth (e2e)", () => {
             .send(data.new)
             .expect(HttpStatus.CREATED)
             .end((err, res) => {
-                if (err) return done(err);
+                if (err) return done(err)
 
-                const { message } = res.body as Record<string | number, string>;
+                const { message } = res.body as Record<string | number, string>
 
-                expect(message).toEqual("Check your e-mail");
+                expect(message).toEqual("Check your e-mail")
 
-                done();
-            });
-    });
+                done()
+            })
+    })
 
     it("Server should response duplication error when client registers with already used email or name", done => {
         return request(app.getHttpServer())
@@ -61,14 +61,14 @@ describe("Auth (e2e)", () => {
                 const { message, statusCode } = res.body as Record<
                     string | number,
                     string
-                >;
+                >
 
-                expect(message).toBe("This email is already using");
-                expect(statusCode).toBe(HttpStatus.CONFLICT);
+                expect(message).toBe("This email is already using")
+                expect(statusCode).toBe(HttpStatus.CONFLICT)
 
-                done();
-            });
-    });
+                done()
+            })
+    })
 
     it("Server should authorize by sending token when client sends to email and password", done => {
         return request(app.getHttpServer())
@@ -76,27 +76,27 @@ describe("Auth (e2e)", () => {
             .send(data.sign)
             .expect(HttpStatus.CREATED)
             .end((err, res) => {
-                if (err) return done(err);
+                if (err) return done(err)
 
                 const {
                     message,
                     access_token,
                     refresh_token,
                     expires_in,
-                } = res.body as Record<string | number, string>;
+                } = res.body as Record<string | number, string>
 
-                expect(message).toEqual("Successfully Authorized");
+                expect(message).toEqual("Successfully Authorized")
 
-                expect(typeof access_token).toBe("string");
-                expect(typeof refresh_token).toBe("string");
-                expect(typeof expires_in).toBe("number");
+                expect(typeof access_token).toBe("string")
+                expect(typeof refresh_token).toBe("string")
+                expect(typeof expires_in).toBe("number")
 
-                token = access_token;
-                refreshToken = refresh_token;
+                token = access_token
+                refreshToken = refresh_token
 
-                done();
-            });
-    });
+                done()
+            })
+    })
 
     it("Server should sends new token it generates when client wants to renew token", done => {
         return request(app.getHttpServer())
@@ -104,21 +104,21 @@ describe("Auth (e2e)", () => {
             .send({ refresh_token: refreshToken })
             .expect(HttpStatus.CREATED)
             .end((err, res) => {
-                if (err) return done(err);
+                if (err) return done(err)
 
                 const {
                     access_token,
                     refresh_token,
                     expires_in,
-                } = res.body as Record<string | number, string>;
+                } = res.body as Record<string | number, string>
 
-                expect(typeof access_token).toBe("string");
-                expect(typeof refresh_token).toBe("string");
-                expect(typeof expires_in).toBe("number");
+                expect(typeof access_token).toBe("string")
+                expect(typeof refresh_token).toBe("string")
+                expect(typeof expires_in).toBe("number")
 
-                done();
-            });
-    });
+                done()
+            })
+    })
 
     it("Server should sends payload of token to client when client makes request 'auth/whoami' with user token", done => {
         return request(app.getHttpServer())
@@ -126,22 +126,22 @@ describe("Auth (e2e)", () => {
             .auth(token, { type: "bearer" })
             .expect(HttpStatus.OK)
             .end((err, res) => {
-                if (err) return done(err);
+                if (err) return done(err)
 
                 const { id, email, flags } = res.body as Record<
                     string | number,
                     string
-                >;
+                >
 
-                expect(typeof id).toBe("string");
-                expect(typeof email).toBe("string");
-                expect(typeof flags).toBe("number");
+                expect(typeof id).toBe("string")
+                expect(typeof email).toBe("string")
+                expect(typeof flags).toBe("number")
 
-                done();
-            });
-    });
+                done()
+            })
+    })
 
     afterAll(async () => {
-        await app.close();
-    });
-});
+        await app.close()
+    })
+})
