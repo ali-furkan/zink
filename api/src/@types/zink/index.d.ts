3bd93cdcedea590f750548817cc43a8f34fe727c
ali-furkan <a.furkan.2003@gmail.com>
2021-08-12 01:27:00 +0300

    :label:(api): add config types

--- api/src/@types/zink/index.d.ts
+++ api/src/@types/zink/index.d.ts
@@ -1,63 +1,68 @@
-export {};
+export {}
 
 declare global {
     namespace Zink {
-        type TConfig = {
-            NODE_ENV: string;
-            port: number;
-            mongodbURI: string;
-            secret: string;
-            rootPath: string;
-            isProd: boolean;
-            flyioToken: string;
-            sgKey: string;
-            mail: string;
-            UUID_NAMESPACE;
-            Firebase: {
-                apiKey: string;
-                authDomain: string;
-                databaseURL: string;
-                projectId: string;
-                storageBucket: string;
-                messagingSenderId: string;
-                appId: string;
-                measurementId: string;
-            };
-        };
-
-        type MatchTypes = "duel" | "catch" | "fast-finger" | "math";
+        type MatchTypes = "duel" | "catch" | "fast-finger" | "math"
 
         interface MatchEntity {
-            id: string;
-            type: MatchTypes;
-            status: boolean;
-            winner: { id: string };
+            id: string
+            type: MatchTypes
+            status: boolean
+            winner: { id: string }
             users: Array<{
-                id: number;
-            }>;
+                id: number
+            }>
         }
 
         interface IToken {
-            id: string;
-            email: string;
-            flags: number;
+            id: string
+            email: string
+            flags: number
         }
 
         interface Response {
-            message?: string;
-            [propName: string]: any;
+            message?: string
+            [propName: string]: any
         }
 
         interface RequestUser extends IToken {
-            iat: number;
-            exp: number;
+            iat: number
+            exp: number
         }
 
         interface AssetsUpFile {
-            fieldname: string;
-            originalname: string;
-            encoding: string;
-            buffer: Buffer;
+            fieldname: string
+            originalname: string
+            encoding: string
+            buffer: Buffer
+        }
+
+        interface AppConfiguration {
+            port: number
+            rootPath: string
+            uuidNamespace: string
+            env: string
+            jwtSecret: string
+        }
+
+        interface DBConfiguration {
+            uri: string
+        }
+
+        interface FBConfiguration {
+            apiKey: string
+            authDomain: string
+            databaseURL: string
+            projectId: string
+            storageBucket: string
+            messagingSenderId: string
+            appId: string
+            measurementId: string
+        }
+
+        interface MailConfiguration {
+            senderMail: string
+            sgKey: string
         }
     }
 }
