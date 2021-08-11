52b2f2c8099fcdf69ae7d34a20d5e9ab48f10486
ali-furkan <a.furkan.2003@gmail.com>
2021-08-11 18:08:13 +0300

    :art:: improve match module

--- api/src/api/matches/match.module.ts
+++ api/src/api/matches/match.module.ts
@@ -1,16 +1,13 @@
-import { Module, forwardRef } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { MatchController } from "./match.controller";
-import { MatchService } from "./match.service";
-import { UserEntity } from "../users/user.entity";
-import { MatchEntity } from "../matches/match.entity";
-import { UsersModule } from "../users/user.module";
+import { Module, forwardRef } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { AuthModule } from "@/auth/auth.module"
+import { UserEntity } from "@/api/users/user.entity"
+import { MatchController } from "./match.controller"
+import { MatchService } from "./match.service"
+import { MatchEntity } from "./match.entity"
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([MatchEntity, UserEntity]),
-        forwardRef(() => UsersModule),
-    ],
+    imports: [TypeOrmModule.forFeature([MatchEntity, UserEntity]), AuthModule],
     controllers: [MatchController],
     providers: [MatchService],
     exports: [TypeOrmModule],
--- api/src/api/matches/match.service.ts
+++ api/src/api/matches/match.service.ts
@@ -4,20 +4,17 @@ import {
     HttpStatus,
     ForbiddenException,
     NotFoundException,
-    Inject,
-    forwardRef,
     BadRequestException,
     Logger,
-} from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { plainToClass } from "class-transformer";
-import { MongoRepository } from "typeorm";
-import { v4 as uuidv4 } from "uuid";
-import { UsersService } from "../users/user.service";
-import { UserEntity } from "../users/user.entity";
-import { MatchEntity } from "./match.entity";
-import { Flag } from "../../auth/flag.decorator";
-import { CreateMatchDTO } from "./dto";
+} from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { plainToClass } from "class-transformer"
+import { MongoRepository } from "typeorm"
+import { v4 as uuidv4 } from "uuid"
+import { FlagService, Flag } from "@/auth/flag.service"
+import { UserEntity } from "@/api/users/user.entity"
+import { MatchEntity } from "./match.entity"
+import { CreateMatchDTO } from "./dto"
 
 @Injectable()
 export class MatchService {
@@ -26,8 +23,7 @@ export class MatchService {
         private readonly matchRepository: MongoRepository<MatchEntity>,
         @InjectRepository(UserEntity)
         private readonly userRepository: MongoRepository<UserEntity>,
-        @Inject(forwardRef(() => UsersService))
-        private readonly userService: UsersService,
+        private readonly flagService: FlagService,
     ) {}
 
     async createMatch({
@@ -41,29 +37,32 @@ export class MatchService {
                 throw new HttpException(
                     `${id} Invalid Match ID`,
                     HttpStatus.BAD_REQUEST,
-                );
-        const uid = id || uuidv4();
+                )
+        const uid = id || uuidv4()
         const usersData = await Promise.all(
             users.map(async id => {
-                const user = await this.userRepository.findOne({ id });
+                const user = await this.userRepository.findOne({ id })
                 if (!user)
-                    throw new BadRequestException(`${id} Invalid User ID`);
-                else return plainToClass(UserEntity, user);
+                    throw new BadRequestException(`${id} Invalid User ID`)
+                else return plainToClass(UserEntity, user)
             }),
-        );
+        )
+
         const match = await this.matchRepository.create({
             id: uid,
             type,
             status: status || true,
             users,
-        });
-        await this.matchRepository.save(match);
+        })
+        await this.matchRepository.save(match)
+
         Logger.log(
             `Created Match {"${id}", ${type.toUpperCase()}, [${users.join(
                 ",",
             )}]}`,
             "Match Service",
-        );
+        )
+
         return {
             message: "Successfully Created Match",
             status,
@@ -71,33 +70,37 @@ export class MatchService {
             type,
             users: usersData,
             sequence: Array(10).fill(type),
-        };
+        }
     }
 
     async deleteMatch(id: string): Promise<Zink.Response> {
-        const match = await this.matchRepository.deleteOne({ id });
+        const match = await this.matchRepository.deleteOne({ id })
         if (match.result.ok !== 1)
-            throw new NotFoundException("Match Not Found");
-        Logger.log(`Deleted {${id}} Match`, "Match Service");
-        return { message: "Deleted successfully match" };
+            throw new NotFoundException("Match Not Found")
+
+        Logger.log(`Deleted {${id}} Match`, "Match Service")
+
+        return { message: "Deleted successfully match" }
     }
 
     async getMatch({
         id,
         user,
     }: {
-        id: string;
-        user: Zink.RequestUser;
+        id: string
+        user: Zink.RequestUser
     }): Promise<Zink.Response> {
-        const match = await this.matchRepository.findOne({ id });
+        const match = await this.matchRepository.findOne({ id })
         if (
             !match.users.some(id => id === user.id) ||
-            !this.userService.matchFlags(Flag.CREATE_MATCH, user.flags)
+            !this.flagService.isMatchFlag(Flag.CREATE_MATCH, user.flags)
         )
-            throw new ForbiddenException();
+            throw new ForbiddenException()
+
         const users = await match.users.map(
             async id => await this.userRepository.findOne({ id }),
-        );
-        return Object.assign({}, match, { users });
+        )
+
+        return Object.assign({}, match, { users })
     }
 }
