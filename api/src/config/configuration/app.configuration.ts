bcda7b6acda0a241530b6d197af8d93282f8dd0c
ali-furkan <a.furkan.2003@gmail.com>
2021-08-17 01:22:32 +0300

    :art:(api): improve env var for dev

--- api/src/config/configuration/app.configuration.ts
+++ api/src/config/configuration/app.configuration.ts
@@ -3,7 +3,7 @@ import { registerAs } from "@nestjs/config"
 export const AppConfiguration = registerAs("app", () => ({
     port: parseInt(process.env.PORT) || 3000,
     rootPath: process.env.ROOT_PATH,
-    uuidNamespace: process.env.UUID_NAMESPACE,
+    uuidNamespace: process.env.UUID_NAMESPACE || "dev-uuid-namespace",
     env: process.env.NODE_ENV || "development",
-    jwtSecret: process.env.JWT_SECRET || "şakir",
+    jwtSecret: process.env.JWT_SECRET || "dev-secret",
 }))
