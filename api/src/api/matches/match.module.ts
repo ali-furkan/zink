96f9ea48d48ac31f2487b32826dcfa08575942d3
ali-furkan <a.furkan.2003@gmail.com>
2021-08-17 01:21:35 +0300

    :bug:(api): fix auth guard import bugs

--- api/src/api/matches/match.module.ts
+++ api/src/api/matches/match.module.ts
@@ -1,13 +1,20 @@
-import { Module, forwardRef } from "@nestjs/common"
+import { Module, CacheModule } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { AuthModule } from "@/auth/auth.module"
 import { UserEntity } from "@/api/users/user.entity"
+import { UsersModule } from "@/api/users/user.module"
 import { MatchController } from "./match.controller"
 import { MatchService } from "./match.service"
 import { MatchEntity } from "./match.entity"
 
 @Module({
-    imports: [TypeOrmModule.forFeature([MatchEntity, UserEntity]), AuthModule],
+    imports: [
+        TypeOrmModule.forFeature([MatchEntity, UserEntity]),
+        AuthModule,
+        UsersModule,
+        CacheModule.register(),
+        AuthModule,
+    ],
     controllers: [MatchController],
     providers: [MatchService],
     exports: [TypeOrmModule],
--- api/src/api/words/words.module.ts
+++ api/src/api/words/words.module.ts
@@ -1,12 +1,18 @@
-import { Module } from "@nestjs/common"
+import { CacheModule, Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { UsersModule } from "@/api/users/user.module"
+import { AuthModule } from "@/auth/auth.module"
 import { WordEntity } from "./words.entity"
 import { WordController } from "./words.controller"
 import { WordService } from "./words.service"
 
 @Module({
-    imports: [TypeOrmModule.forFeature([WordEntity]), UsersModule],
+    imports: [
+        TypeOrmModule.forFeature([WordEntity]),
+        UsersModule,
+        CacheModule.register(),
+        AuthModule,
+    ],
     controllers: [WordController],
     providers: [WordService],
     exports: [TypeOrmModule],
